b695a3b6d684afca040d0f52c3e097f984050fa7
Zish1805 <72144072+Z-8Bit@users.noreply.github.com>
2022-01-01 12:49:22 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -25,13 +25,13 @@ read timezone
 timedatectl set-ntp true
 timedatectl status
 
-mkfs.fat -F32 -n "BOOT" ${boot}
-mkfs.ext4 -L "ROOT" ${root}
+mkfs.fat -F32 -n "BOOT" "${boot}"
+mkfs.ext4 -L "ROOT" "${root}"
 # mkswap ${swap}
 # swapon ${swap}
-mount ${root} /mnt
+mount "${root}" /mnt
 mkdir /boot/efi
-mount ${boot} /boot/efi
+mount "${boot}" /boot/efi
 
 sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 10/g" /etc/pacman.conf  # Parallel downloads
 
@@ -40,7 +40,7 @@ genfstab -U /mnt >> /mnt/etc/fstab
 arch-chroot /mnt /bin/bash -e <<EOF
 
 # Timezone
-ln -sf /usr/share/zoneinfo/${timezone} /etc/localtime
+ln -sf /usr/share/zoneinfo/"${timezone}" /etc/localtime
 hwclock --systohc
 
 # Locale
@@ -49,11 +49,11 @@ locale-gen
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
 # Hostname
-echo ${hostname} >> /etc/hostname
+echo "${hostname}" >> /etc/hostname
 echo -e "127.0.0.1\tlocalhost" >> /etc/hosts
 echo -e "::1\t\tlocalhost" >> /etc/hosts
 echo -e "127.0.1.1\t${hostname}.localdomain\t${hostname}" >> /etc/hosts
-echo root:${password} | chpasswd
+echo root:"${password}" | chpasswd
 
 # Pacman Configuration
 sed -i "/#Color/a ILoveCandy" /etc/pacman.conf  # Making pacman prettier
@@ -61,24 +61,25 @@ sed -i "s/#Color/Color/g" /etc/pacman.conf  # Add color to pacman
 sed -i "s/#ParallelDownloads = 5/ParallelDownloads = 10/g" /etc/pacman.conf  # Parallel downloads
 
 # Package Downloading
-pacman -S efibootmgr vim networkmanager network-manager-applet wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi gvfs os-prober ntfs-3g bluez bluez-utils git neofetch --noconfirm
+pacman -S efibootmgr vim networkmanager network-manager-applet wpa_supplicant mtools dosfstools reflector base-devel linux-headers avahi gvfs os-prober ntfs-3g bluez bluez-utils git neofetch  --noconfirm
 
 # grub-install --target=x86_64-efi --efi-directory=/boot/efi --bootloader-id=GRUB 
 # grub-install --target=x86_64-efi  --bootloader-id=Arch --recheck
 # grub-mkconfig -o /boot/grub/grub.cfg
 # echo "GRUB_DISABLE_OS_PROBER=false" >> /etc/default/grub 
 # grub-mkconfig -o /boot/grub/grub.cfg
+# refind-install
 
 for service in bluetooth NetworkManager; do
-  systemctl enable --now $service
+  systemctl enable $service
 done
 
 # Username
-useradd -m ${username}
+useradd -m "${username}"
 echo "${username}":"${password}" | chpasswd
-usermod -aG wheel,audio,video,optical,storage "${username}"
+usermod -aG wheel,audio,video,storage,input ${username}
 
-echo "${username} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/${username}
+echo "${username} ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers.d/"${username}"
 
 EOF
 
